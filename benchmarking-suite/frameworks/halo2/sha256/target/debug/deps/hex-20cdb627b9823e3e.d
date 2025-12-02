971427ae6c508bcdfb2be528c19e9f2c4e43645b
Abhivansh <31abhivanshj@gmail.com>
2025-12-02 22:21:04 +0530

    Revert "wip"
    This reverts commit 3b400f1cb6f25ff3780b33608f0dde7f9e395305.

--- benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/hex-20cdb627b9823e3e.d
+++
@@ -1,8 +0,0 @@
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/hex-20cdb627b9823e3e.d: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/error.rs
-
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/libhex-20cdb627b9823e3e.rlib: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/error.rs
-
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/libhex-20cdb627b9823e3e.rmeta: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/error.rs
-
-/home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/lib.rs:
-/home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/hex-0.4.3/src/error.rs:
--- benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/uint-b2e0e999b1a7c6bd.d
+++
@@ -1,8 +0,0 @@
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/uint-b2e0e999b1a7c6bd.d: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/uint.rs
-
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/libuint-b2e0e999b1a7c6bd.rlib: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/uint.rs
-
-/home/akronim26/Desktop/Skills/Blockchain/Projects/Deimos/benchmarking-suite/frameworks/halo2/sha256/target/debug/deps/libuint-b2e0e999b1a7c6bd.rmeta: /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/lib.rs /home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/uint.rs
-
-/home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/lib.rs:
-/home/akronim26/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/uint-0.9.5/src/uint.rs:
